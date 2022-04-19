8a25a8c3b16244c0a6ea2637b0e9041baffd5fa8
MAO-COMMUNITY <78520987+mao2116@users.noreply.github.com>
2022-04-19 08:58:51 +0600

    Update mao.py

--- mao.py
+++ mao.py
@@ -6,9 +6,6 @@ def __init__():
     if bit == '64bit':
         from mao_main import defultchk
         defultchk()
-    '''elif bit == '32bit':
-        from fuck_mao_32 import menu
-        menu()'''
     else:
         print(bit)
         exit()
